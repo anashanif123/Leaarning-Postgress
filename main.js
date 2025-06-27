f745964599ceb50a2c755fb44860f5c5bc10661d
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-27 18:49:50 +0500

    fetch All Data And Fetch thorough Id is Done

--- main.js
+++ main.js
@@ -5,33 +5,64 @@ const app = express();
 app.use(express.json());
 
 const connection = new Client({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-    password:"Anas1234",
-    database:"demo"
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "Anas1234",
+    database: "demo"
 });
 
 connection.connect().then(() => console.log("Connected to the database successfully!"))
 
 app.post("/add", async (req, res) => {
-    const {name,id}= req.body;
-    
-    const insert_Query='INSERT INTO demotable (name, id) VALUES ($1, $2)';
+    const { name, id } = req.body;
 
-   connection.query(insert_Query, [name, id],(err, result) => {
+    const insert_Query = 'INSERT INTO demotable (name, id) VALUES ($1, $2)';
+
+    connection.query(insert_Query, [name, id], (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send("Error inserting data");
-            }
+        }
         else {
             console.log(result);
             res.status(200).send("Data inserted successfully");
         }
 
-})
+    })
 });
+
+app.get('/fetchData',(req,res)=>{
+
+
+    const fetch_query = 'SELECT * FROM demotable';
+    connection.query(fetch_query, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error fetching data");
+        } else {
+            res.status(200).json(result.rows);
+        }
+    });
+
+})
+
+app.get('/fetchData/:id', (req, res) => {
+    const id =req.params.id;
+    const fetch_query = 'SELECT * FROM demotable WHERE id = $1';
+    connection.query(fetch_query, [id], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error fetching data");
+        } else {
+             console.log(result.rows);
+             res.send(result.rows);
+             
+        }
+    });
+})
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
-}   );
+});
